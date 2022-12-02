25f8b8a742665ed7b2bb8917042b758393ff5b68
Sam Kearney <samuelmkearney@gmail.com>
2022-12-01 20:45:31 -0600

    Format code

--- day_1/src/main.rs
+++ day_1/src/main.rs
@@ -4,7 +4,9 @@ use std::path::Path;
 
 // Returns an Iterator to the Reader of the lines of the file.
 fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
-where P: AsRef<Path>, {
+where
+    P: AsRef<Path>,
+{
     let file = File::open(filename)?;
     Ok(io::BufReader::new(file).lines())
 }
@@ -18,16 +20,26 @@ fn main() {
     for line in lines {
         match line {
             Ok(text) => match text.as_str() {
-                "" => { elves.push(current_calories); current_calories = 0; }
-                _ => { current_calories += text.parse::<i32>().unwrap_or_else(|_| panic!("Encountered a non-numeric value {}", text)) }
+                "" => {
+                    elves.push(current_calories);
+                    current_calories = 0;
+                }
+                _ => {
+                    current_calories += text
+                        .parse::<i32>()
+                        .unwrap_or_else(|_| panic!("Encountered a non-numeric value {}", text))
+                }
             },
-            Err(_) => break
+            Err(_) => break,
         }
     }
 
     assert!(elves.len() >= 3);
 
     elves.sort();
-    println!("Largest number of calories: {}", elves[elves.len()-1]);
-    println!("Sum of top three: {}", elves[elves.len() - 1] + elves[elves.len() - 2] + elves[elves.len() - 3])
+    println!("Largest number of calories: {}", elves[elves.len() - 1]);
+    println!(
+        "Sum of top three: {}",
+        elves[elves.len() - 1] + elves[elves.len() - 2] + elves[elves.len() - 3]
+    )
 }
