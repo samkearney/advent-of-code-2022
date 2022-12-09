f411adbc41b37eea445547fb0bd1ac6d979726c1
Sam Kearney <samuelmkearney@gmail.com>
2022-12-08 20:43:29 -0600

    Day 6 part 2

--- day_6/src/main.rs
+++ day_6/src/main.rs
@@ -1,25 +1,26 @@
 use std::collections::HashSet;
 
-use itertools::Itertools;
-
 fn main() {
     let input_buf = std::fs::read_to_string("input.txt").expect("Couldn't open input.txt for reading");
 
-    println!("{}", get_startcode_index(&input_buf));
+    println!("Start-of-packet: {}", get_marker(&input_buf, 4));
+    println!("Start-of-message: {}", get_marker(&input_buf, 14));
 }
 
-fn get_startcode_index(input_buf: &str) -> usize {
-    for (index, (first, second, third, fourth)) in input_buf.chars().tuple_windows().enumerate() {
-        let mut set = HashSet::new();
-        set.insert(first);
-        set.insert(second);
-        set.insert(third);
-        set.insert(fourth);
+fn get_marker(input_buf: &str, num_unique: usize) -> usize {
+    let mut iter = input_buf.chars().enumerate();
 
-        if set.len() == 4 {
-            return index + 4;
+    while let Some((index, char)) = iter.next() {
+        let mut set = HashSet::new();
+        set.insert(char);
+        for (_, char) in iter.clone().take(num_unique - 1) {
+            set.insert(char);
+        }
+
+        if set.len() == num_unique {
+            return index + num_unique;
         }
     }
 
-    panic!("Start code not found!");
+    panic!("Marker not found!");
 }
