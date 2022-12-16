e9bb4821551ea39c1302c0e71d24d7e1d9117526
Sam Kearney <samuelmkearney@gmail.com>
2022-12-15 18:43:30 -0600

    Day 11 Part 2

--- day_11/src/main.rs
+++ day_11/src/main.rs
@@ -1,28 +1,39 @@
+use std::fs::File;
+use std::io;
+
+type Item = i64;
+
 struct ThrowOperation {
-    item: i32,
+    item: Item,
     target: usize,
 }
 
+enum WorryIncreaseOperation {
+    Add(Item),
+    Mult(Item),
+    Square,
+}
+
 struct Monkey {
-    items: Vec<i32>,
-    op: Box<dyn Fn(i32) -> i32>,
-    test_divisor: i32,
+    items: Vec<Item>,
+    op: WorryIncreaseOperation,
+    test_divisor: Item,
     true_target: usize,
     false_target: usize,
-    times_inspected: u32,
+    times_inspected: u64,
 }
 
 impl Monkey {
     pub fn new(
-        items: Vec<i32>,
-        op: impl Fn(i32) -> i32 + 'static,
-        test_divisor: i32,
+        items: Vec<Item>,
+        op: WorryIncreaseOperation,
+        test_divisor: Item,
         true_target: usize,
         false_target: usize,
     ) -> Self {
         Monkey {
             items,
-            op: Box::new(op),
+            op,
             test_divisor,
             true_target,
             false_target,
@@ -30,21 +41,33 @@ impl Monkey {
         }
     }
 
-    pub fn times_inspected(&self) -> u32 {
+    pub fn times_inspected(&self) -> u64 {
         self.times_inspected
     }
 
-    pub fn process_round(&mut self) -> Vec<ThrowOperation> {
-        self.times_inspected += self.items.len() as u32;
+    pub fn test_divisor(&self) -> Item {
+        self.test_divisor
+    }
+
+    pub fn process_round(&mut self, common_mult: Option<i64>) -> Vec<ThrowOperation> {
+        self.times_inspected += self.items.len() as u64;
 
         self.items
             .drain(..)
             .map(|item| {
                 // Inspect: Increase worry level
-                let item = (self.op)(item);
+                let item = match self.op {
+                    WorryIncreaseOperation::Add(addend) => item + addend,
+                    WorryIncreaseOperation::Mult(multiplier) => item * multiplier,
+                    WorryIncreaseOperation::Square => item * item,
+                };
 
                 // Get bored: Divide worry level
-                let item = item / 3;
+                let item = if let Some(common_mult) = common_mult {
+                    item % common_mult
+                } else {
+                    item / 3
+                };
 
                 // Choose target
                 let target = if item % self.test_divisor == 0 {
@@ -58,48 +81,127 @@ impl Monkey {
             .collect()
     }
 
-    pub fn catch_item(&mut self, item: i32) {
+    pub fn catch_item(&mut self, item: Item) {
         self.items.push(item);
     }
 }
 
 fn main() {
-    let mut monkeys = [
-        Monkey::new(vec![75, 75, 98, 97, 79, 97, 64], |old| old * 13, 19, 2, 7),
-        Monkey::new(vec![50, 99, 80, 84, 65, 95], |old| old + 2, 3, 4, 5),
-        Monkey::new(
-            vec![96, 74, 68, 96, 56, 71, 75, 53],
-            |old| old + 1,
-            11,
-            7,
-            3,
-        ),
-        Monkey::new(vec![83, 96, 86, 58, 92], |old| old + 8, 17, 6, 1),
-        Monkey::new(vec![99], |old| old * old, 5, 0, 5),
-        Monkey::new(vec![60, 54, 83], |old| old + 4, 2, 2, 0),
-        Monkey::new(vec![77, 67], |old| old * 17, 13, 4, 1),
-        Monkey::new(vec![95, 65, 58, 76], |old| old + 5, 7, 3, 6),
-    ];
+    let mut monkeys = parse_input();
 
     for _ in 0..20 {
         for monkey_idx in 0..monkeys.len() {
-            let throws = monkeys[monkey_idx].process_round();
+            let throws = monkeys[monkey_idx].process_round(None);
             for throw in throws {
                 monkeys[throw.target].catch_item(throw.item);
             }
         }
     }
 
-    let mut inspection_totals: Vec<u32> = monkeys
+    let mut inspection_totals: Vec<u64> = monkeys
         .iter()
         .map(|monkey| monkey.times_inspected())
         .collect();
     inspection_totals.sort();
 
-    println!("{:?}", inspection_totals);
     println!(
-        "Monkey business: {}",
+        "Monkey business for part one: {}",
         inspection_totals[inspection_totals.len() - 1]
             * inspection_totals[inspection_totals.len() - 2]
     );
+
+    let mut monkeys = parse_input();
+    let common_mult = monkeys
+        .iter()
+        .fold(1, |accum, monkey| accum * monkey.test_divisor());
+
+    for _ in 0..10000 {
+        for monkey_idx in 0..monkeys.len() {
+            let throws = monkeys[monkey_idx].process_round(Some(common_mult));
+            for throw in throws {
+                monkeys[throw.target].catch_item(throw.item);
+            }
+        }
+    }
+
+    let mut inspection_totals: Vec<u64> = monkeys
+        .iter()
+        .map(|monkey| monkey.times_inspected())
+        .collect();
+    inspection_totals.sort();
+
+    println!(
+        "Monkey business for part two: {}",
+        inspection_totals[inspection_totals.len() - 1]
+            * inspection_totals[inspection_totals.len() - 2]
+    );
+}
+
+fn parse_input() -> Vec<Monkey> {
+    let mut result = Vec::new();
+
+    let mut input = aoc::read_lines("input.txt").expect("Couldn't open input.txt for reading");
+
+    while let Some(Ok(text)) = input.next() {
+        if text.starts_with("Monkey") {
+            result.push(get_monkey(&mut input));
+        }
+    }
+
+    result
+}
+
+fn get_monkey(input: &mut io::Lines<io::BufReader<File>>) -> Monkey {
+    let starting_items: Vec<Item> = get_line_required(input)
+        .strip_prefix("  Starting items: ")
+        .unwrap()
+        .split(',')
+        .map(|str| str.trim())
+        .map(|str| str.parse().unwrap())
+        .collect();
+
+    let operation_line = get_line_required(input);
+    let operation_str = operation_line
+        .strip_prefix("  Operation: new = old ")
+        .unwrap();
+    let worry_operation = if operation_str == "* old" {
+        WorryIncreaseOperation::Square
+    } else if operation_str.starts_with('*') {
+        WorryIncreaseOperation::Mult(operation_str.strip_prefix("* ").unwrap().parse().unwrap())
+    } else if operation_str.starts_with('+') {
+        WorryIncreaseOperation::Add(operation_str.strip_prefix("+ ").unwrap().parse().unwrap())
+    } else {
+        panic!("Unknown operation {}", operation_str);
+    };
+
+    let test_divisor: Item = get_line_required(input)
+        .strip_prefix("  Test: divisible by ")
+        .unwrap()
+        .parse()
+        .unwrap();
+    let true_target: usize = get_line_required(input)
+        .strip_prefix("    If true: throw to monkey ")
+        .unwrap()
+        .parse()
+        .unwrap();
+    let false_target: usize = get_line_required(input)
+        .strip_prefix("    If false: throw to monkey ")
+        .unwrap()
+        .parse()
+        .unwrap();
+
+    Monkey::new(
+        starting_items,
+        worry_operation,
+        test_divisor,
+        true_target,
+        false_target,
+    )
+}
+
+fn get_line_required(input: &mut io::Lines<io::BufReader<File>>) -> String {
+    input
+        .next()
+        .expect("Malformed input: Expected line")
+        .expect("Malformed input: expected line")
 }
